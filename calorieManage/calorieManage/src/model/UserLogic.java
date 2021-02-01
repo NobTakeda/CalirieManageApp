bfbd3cd40416f39e19377ef4b71d7b356a4e97cb
Ntakeda <muzat.yakuzajeep@gmail.com>
2021-02-01 12:55:47 +0900

    ファイル位置修正

--- calorieManage/calorieManage/src/model/UserLogic.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class UserLogic {
-	public void execute(User user) {
-		Double bmi=(user.getWeight()/((user.getHeight()/100)*(user.getHeight()/100)));
-		bmi= ((double)Math.round(bmi * 10))/10;
-		user.setBmi(bmi);
-	}
-}
